12798c3f8f7368f6e80f9dacee8c8fe68c46c695
ROSHAN20172 <np03cs4a220020@heraldcollege.edu.np>
2023-05-22 09:08:34 +0545

    prototype 3 of weather site completed

--- Roshan Kumar Sah_2329235.js
+++ Roshan Kumar Sah_2329235.js
@@ -1,67 +1,143 @@
+// Define the API key and URL for fetching weather data
 const apikey = "daee0ae2ee355e95216a143b6c38f70c";
 const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+// Select DOM elements
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-
 const weatherIcon = document.querySelector(".weather-icon");
 
-let city = "mobile";
+// Function to check weather for a city
+async function checkWeather(city) {
+  // Hide error message and display weather and details sections
+  if (document.querySelector(".error").style.display === "block") {
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".details").style.display = "flex";
+  }
 
-async function checkWeather(city){
-    if (document.querySelector(".error").style.display == "block") {
-        document.querySelector(".error").style.display = "none";
-        document.querySelector(".weather").style.display = "block";
-        document.querySelector(".details").style.display = "flex";
+  // Check if cached weather data exists for the city
+  const cachedWeatherData = localStorage.getItem(city);
+  if (cachedWeatherData) {
+    // Retrieve cached data and timestamp
+    const { data, timestamp } = JSON.parse(cachedWeatherData);
+    updateWeatherData(data, "Data Accessed from Local Storage");
+
+    // Check if device is online and cached data is old
+    if (navigator.onLine && isDataOld(timestamp)) {
+      fetchAndUpdateWeatherData(city);
     }
-
-    const response = await fetch(apiurl + city + `&appid=${apikey}`);
-
-    if(response.status == 404){
-        document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
-        document.querySelector(".details").style.display = "none";
-    }
-    else{
-        var data = await response.json();
-
-        console.log(data);
-
-        const options = { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
-        document.querySelector(".date").innerHTML = new Date().toLocaleDateString('en-US', options);
-
-        document.querySelector(".temp").innerHTML = data.main.temp + "&deg;C";
-        document.querySelector(".city").innerHTML = data.name + ", " + data.sys.country;
-        document.querySelector(".pressure").innerHTML = data.main.pressure + " hPa" + "<br>Pressure";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%" +"<br>Humidity";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h" +"<br>Wind Speed";
-        document.querySelector(".icon-name").innerHTML = data.weather[0].main;
-        document.querySelector(".rainfall").innerHTML = data.rain ? data.rain["1h"] + " mm/h"+"<br>Rainfall" : "N/A"+"<br>Rainfall";
-
-        document.querySelector(".condition").innerHTML = data.weather[0].description + "<br>Condition";
-        document.querySelector(".conditionimg").innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}">`;
-
-        if(data.weather[0].main == "Clouds"){
-            weatherIcon.src = "Roshan Kumar Sah_2329235_Clouds.png";
-        }
-        else if(data.weather[0].main == "Clear"){
-            weatherIcon.src = "Roshan Kumar Sah_2329235_Clear.png";
-        }
-        else if(data.weather[0].main == "Rain"){
-            weatherIcon.src = "Roshan Kumar Sah_2329235_Rain.png";
-        }
-        else if(data.weather[0].main == "Drizzle"){
-            weatherIcon.src = "Roshan Kumar Sah_2329235_Drizzle.png";
-        }
-        else if(data.weather[0].main == "Mist"){
-            weatherIcon.src = "Roshan Kumar Sah_2329235_Mist.png";
-        }
-        document.querySelector(".error").style.display = "none";
-    }  
+  } else if (navigator.onLine) {
+    // Fetch weather data from the API and update the cache
+    fetchAndUpdateWeatherData(city);
+  } else {
+    showError("No internet connection. Failed to fetch weather data.");
+  }
 }
 
-checkWeather(city);
+// Function to check if cached data is old (older than 10 minutes)
+function isDataOld(timestamp) {
+  const TEN_MINUTES = 10 * 60 * 1000; // 10 minutes in milliseconds
+  const currentTime = new Date().getTime();
+  return currentTime - timestamp >= TEN_MINUTES;
+}
 
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-})
+// Function to fetch weather data from the API and update the cache
+async function fetchAndUpdateWeatherData(city) {
+  try {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    if (response.status === 404) {
+      showError("City not found.");
+      console.log("Invalid city name");
+    } else {
+      const data = await response.json();
+      const timestamp = new Date().getTime();
+      localStorage.setItem(city, JSON.stringify({ data, timestamp }));
+      updateWeatherData(data, "Data Accessed from Internet");
+    }
+  } catch (error) {
+    showError("Failed to fetch weather data.");
+  }
+}
+
+// Function to update the weather data on the webpage
+function updateWeatherData(data, source) {
+  // Display the current date
+  const options = {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  document.querySelector(".date").innerHTML = new Date().toLocaleDateString(
+    "en-US",
+    options
+  );
+
+  // Update the weather details on the webpage
+  document.querySelector(".temp").innerHTML = data.main.temp + "&deg;C";
+  document.querySelector(".city").innerHTML =
+    data.name + ", " + data.sys.country;
+  document.querySelector(".pressure").innerHTML =
+    data.main.pressure + " hPa" + "<br>Pressure";
+  document.querySelector(".humidity").innerHTML =
+    data.main.humidity + "%" + "<br>Humidity";
+  document.querySelector(".wind").innerHTML =
+    data.wind.speed + " Km/h" + "<br>Wind Speed";
+  document.querySelector(".icon-name").innerHTML = data.weather[0].main;
+  document.querySelector(".rainfall").innerHTML = data.rain
+    ? data.rain["1h"] + " mm/h" + "<br>Rainfall"
+    : "N/A" + "<br>Rainfall";
+
+  document.querySelector(".condition").innerHTML =
+    data.weather[0].description + "<br>Condition";
+  document.querySelector(
+    ".conditionimg"
+  ).innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}">`;
+
+  // Set the weather icon based on the weather condition
+  if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "Roshan Kumar Sah_2329235_Clouds.png";
+  } else if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "Roshan Kumar Sah_2329235_Clear.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "Roshan Kumar Sah_2329235_Rain.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "Roshan Kumar Sah_2329235_Drizzle.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "Roshan Kumar Sah_2329235_Mist.png";
+  }
+
+  // Hide the error message
+  document.querySelector(".error").style.display = "none";
+  console.log(source);
+}
+
+// Function to show error message
+function showError(message) {
+  document.querySelector(".error").style.display = "block";
+  document.querySelector(".weather").style.display = "none";
+  document.querySelector(".details").style.display = "none";
+  document.querySelector(".error").innerHTML = message;
+}
+
+// Initial weather check for a specific city
+checkWeather("mobile");
+
+// Event listener for search button
+searchBtn.addEventListener("click", () => {
+  const city = searchBox.value;
+  if (city.trim() !== "") {
+    checkWeather(city);
+  }
+});
+
+// Event listener for Enter key press
+searchBox.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    const city = searchBox.value;
+    if (city.trim() !== "") {
+      checkWeather(city);
+    }
+  }
+});
--- Roshan Kumar Sah_2329235.php
+++ Roshan Kumar Sah_2329235.php
@@ -18,7 +18,7 @@
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			margin-top: 50px;
+			margin-top: 30px;
 		}
 		input[type="text"] {
 			padding: 10px;
@@ -60,34 +60,59 @@
 			height: 50px;
 			width: 50px;
 		}
+	    .homepage {
+	      display: flex;
+	      justify-content: center;
+	      margin-top: 20px;
+	    }
+	    .homepage button {
+	      padding: 12px 20px;
+	      background-color: red;
+	      color: #ffffff;
+	      border: none;
+	      border-radius: 5px;
+	      font-size: 18px;
+	      text-decoration: none;
+	      cursor: pointer;
+	      transition: background-color 0.3s ease;
+	    }
+	    .homepage button:hover {
+	      background-color: darkred;
+	    }
+	    .homepage button a {
+	      color: inherit;
+	      text-decoration: none;
+	    }
 	</style>
 </head>
 <body>
 
-	<form method="get" action="">
-		<input type="text" name="city" placeholder="Enter city name">
-		<input type="submit" name="submit" value="Search">
-	</form>
-
+<!-- HTML form for user input -->
+<form method="get" action="">
+  <input type="text" name="city" placeholder="Enter city name">
+  <input type="submit" name="submit" value="Search">
+</form>
 
 <?php
+// PHP code for retrieving weather data, storing it in the database, and displaying it on the page
 
-if (isset($_GET['submit'])) {
-  $city = $_GET['city'];
-} else {
-  $city = "mobile";
-}
+// Retrieve the city name from the form or set a default value
+$city = isset($_GET['submit']) ? $_GET['city'] : "mobile";
 
-
+// Construct the API URL for retrieving weather data based on the city name
 $url = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid=daee0ae2ee355e95216a143b6c38f70c&units=metric";
 
+// Retrieve the weather data from the API
 $response = file_get_contents($url);
 $data = json_decode($response, true);
 
-if (!$data) {
-  die("Error: Failed to retrieve data from OpenWeatherMap API.");
+// Check if the data retrieval was successful and handle errors
+if (!$data || $data['cod'] != 200) {
+  die("Error: Failed to retrieve data from OpenWeatherMap API. <br> 
+  Please enter a valid city name.");
 }
 
+// Extract the relevant weather information from the retrieved data
 $city_name = $data['name'];
 $condition = $data['weather'][0]['main'];
 $icon = $data['weather'][0]['icon'];
@@ -95,73 +120,102 @@ $temperature = $data['main']['temp'];
 $pressure = $data['main']['pressure'];
 $humidity = $data['main']['humidity'];
 $wind_speed = $data['wind']['speed'];
-$wind_direction = $data['wind']['deg'];
-$cloudiness = $data['clouds']['all'];
-$sunrise = date('Y-m-d H:i:s', $data['sys']['sunrise']);
-$sunset = date('Y-m-d H:i:s', $data['sys']['sunset']);
-$rainfall = isset($data['rain']['1h']) ? $data['rain']['1h'] : 'not given';
 
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'prototype_2';
+// Database connection details
+$host = 'sql311.epizy.com';
+$username = 'epiz_34175099';
+$password = 'Txao0EYv1n5MB';
+$dbname = 'epiz_34175099_prototype_2';
 
-$conn = mysqli_connect($hostname, $username, $password, $dbname);
+// Create a connection to the database
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
+// Check if the connection was successful
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM weather WHERE `city`='$city_name' AND DATE(`date`) = CURDATE()";
+$dataFromDatabase = false; // Variable to track if data is fetched from the database
 
+// Check if there is existing weather data for the current date and city
+$sql = "SELECT * FROM weather WHERE `city`='$city_name' AND DATE(`date`) = CURDATE()";
 $result = mysqli_query($conn, $sql);
 
+// If there is existing data, update it in the database
 if (mysqli_num_rows($result) > 0) {
-  $sql = "UPDATE weather SET `icon`='$icon', `condition`='$condition', `temperature`='$temperature', `pressure`='$pressure', `humidity`='$humidity', `wind_speed`='$wind_speed' WHERE `city`='$city_name' AND `date`= DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')";
+    $sql = "UPDATE weather SET `condition`='$condition', `icon`='$icon', `pressure`='$pressure', `temperature`='$temperature', `humidity`='$humidity', `wind_speed`='$wind_speed' WHERE `city`='$city_name' AND `date`=NOW()";
+    $dataFromDatabase = true; // Data is fetched from the database
 } else {
-  
-  $sql = "INSERT INTO weather (`city`, `date`, `icon`, `condition`, `temperature`, `pressure`, `wind_speed`, `humidity`)
-        VALUES ('$city_name', NOW(), '$icon', '$condition', '$temperature', '$pressure', '$wind_speed', '$humidity')";
+  // If there is no existing data, insert a new row into the database
+  $sql = "INSERT INTO weather (city, `condition`, icon, temperature, pressure, humidity, wind_speed,  date) 
+  VALUES ('$city_name', '$condition', '$icon', '$temperature', '$pressure', '$humidity', '$wind_speed', NOW())";
 }
 
-
+// Execute the SQL query
 mysqli_query($conn, $sql);
 
+// Retrieve the weather data for the past 7 days from the database
 $sql = "SELECT * FROM weather WHERE `city`='$city_name' ORDER BY `date` DESC LIMIT 7";
 $result = mysqli_query($conn, $sql);
 
-echo "<h1>Weather In {$city_name} (Last 7 Days Record)</h1>";
+// Display the weather data in an HTML table
 echo "<table border='1'>";
-while ($row = mysqli_fetch_assoc($result)) {
-$date = date('Y-m-d', strtotime($row['date']));
-$icon = $row['icon'];
-$condition = $row['condition'];
-$temperature = $row['temperature'];
-$pressure = $row['pressure'];
-$humidity = $row['humidity'];
-$wind_speed = $row['wind_speed'];
-
-echo "<td> Date";
-echo "<td> Icon";
-echo "<td> Condition";
-echo "<td> Temperature";
-echo "<td> pressure";
-echo "<td> Humidity";
-echo "<td> Wind Speed";
-
 echo "<tr>";
-echo "<td>{$date}</td>";
-echo "<td><img src='http://openweathermap.org/img/w/{$icon}.png'></td>";
-echo "<td>{$condition}</td>";
-echo "<td>{$temperature}°C</td>";
-echo "<td>{$pressure} hPa</td>";
-echo "<td>{$humidity}%</td>";
-echo "<td>{$wind_speed} m/s</td>";
+echo "<th>Date</th>";
+echo "<th>City</th>";
+echo "<th>Condition</th>";
+echo "<th>Icon</th>";
+echo "<th>Pressure</th>";
+echo "<th>Temperature</th>";
+echo "<th>Humidity</th>";
+echo "<th>Wind Speed</th>";
 echo "</tr>";
+
+// Iterate over the retrieved weather data and populate the table rows
+while ($row = mysqli_fetch_assoc($result)) {
+  $city = $row['city'];
+  $date = date('Y-m-d', strtotime($row['date']));
+  $condition = $row['condition'];
+  $icon = $row['icon'];
+  $temperature = $row['temperature'];
+  $humidity = $row['humidity'];
+  $wind_speed = $row['wind_speed'];
+
+  echo "<tr>";
+  echo "<td>{$date}</td>";
+  echo "<td>{$city}</td>";
+  echo "<td>{$condition}</td>";
+  echo "<td><img src='http://openweathermap.org/img/w/{$icon}.png'></td>";
+  echo "<td>{$pressure}hPa</td>";
+  echo "<td>{$temperature}°C</td>";
+  echo "<td>{$humidity}%</td>";
+  echo "<td>{$wind_speed} m/s</td>";
+  echo "</tr>";
+}
+
+// Display a message if there is incomplete weather data
+if (mysqli_num_rows($result) < 7) {
+  echo "<tr><td colspan='8'>Some of the weather data are only found.</td></tr>";
 }
 echo "</table>";
 
+// Display a message indicating the source of the weather data
+if ($dataFromDatabase) {
+  echo "<script>console.log('Weather data accessed from database.');</script>";
+} else {
+  echo "<script>console.log('Weather data fetched from internet.');</script>";
+}
+
+// Close the database connection
 mysqli_close($conn);
 ?>
+
+<!-- Homepage button -->
+<div class="homepage">
+  <button>
+    <a href="index.html">Go back to homepage</a>
+  </button>
+</div>
+
 </body>
 </html>
